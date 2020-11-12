7f1c5af10c4b431894a28a28fa0079d18019f23c
mortalshot <seregagl1996@gmail.com>
2020-11-12 14:11:40 +0300

    calculator

---
+++ #src/js/components/calculator.js
@@ -0,0 +1,62 @@
+const computerSlider = document.getElementById('numSlider1');
+const computerInput = document.getElementById('inputNumSlider1');
+
+let computerPrice = 1000;
+let serverPrice = 2000;
+const calculatorResLabel = document.querySelector('.calculator-sum__result span');
+
+noUiSlider.create(computerSlider, {
+    start: [5, 5],
+    connect: true,
+    step: 1,
+    range: {
+        'min': 5,
+        'max': 70
+    },
+    format: wNumb({
+        decimals: 0,
+    }),
+    pips: {
+        mode: 'steps',
+    },
+});
+
+const serverSlider = document.getElementById('numSlider2');
+const serverInput = document.getElementById('inputNumSlider2');
+
+noUiSlider.create(serverSlider, {
+    start: [1, 1],
+    connect: true,
+    step: 1,
+    range: {
+        'min': 1,
+        'max': 70
+    },
+    format: wNumb({
+        decimals: 0,
+    }),
+    pips: {
+        mode: 'steps',
+    },
+});
+
+computerSlider.noUiSlider.on('update', function (values, handle) {
+    computerInput.value = values[handle];
+    priceCalculate(computerPrice, serverPrice, serverInput.value, computerInput.value);
+});
+computerInput.addEventListener('change', function () {
+    computerSlider.noUiSlider.set([5, computerInput.value]);
+    priceCalculate(computerPrice, serverPrice, serverInput.value, computerInput.value);
+});
+serverSlider.noUiSlider.on('update', function (values, handle) {
+    serverInput.value = values[handle];
+    priceCalculate(computerPrice, serverPrice, serverInput.value, computerInput.value);
+});
+serverInput.addEventListener('change', function () {
+    serverSlider.noUiSlider.set([1, serverInput.value]);
+    priceCalculate(computerPrice, serverPrice, serverInput.value, computerInput.value);
+});
+
+function priceCalculate(computerPrice, serverPrice, serverNum, computerNum) {
+    calculatorResLabel.innerHTML = serverPrice * serverNum + computerPrice * computerNum;
+}
--- #src/js/index.js
+++ #src/js/index.js
@@ -1,10 +1,13 @@
 $(document).ready(function () {
     // libs
     @@include('libs/dynamic-adapt.js')
+    @@include('libs/wNumb.js')
+    @@include('libs/nouislider.js')
 
     // components
     @@include('components/webp.js')
     @@include('components/menu.js')
+    @@include('components/calculator.js')
 });
 
 @@include('components/header-fixed.js')
