e434179ed69a56de288e80b0e7e6a62228eff75d
mortalshot <seregagl1996@gmail.com>
2020-11-12 20:30:42 +0300

    calculator and it-culture section

---
+++ #src/js/components/slider.js
@@ -0,0 +1,30 @@
+$('.reviews__slider').slick({
+    arrows: true,
+    slidesToShow: 2,
+    variableWidth: true,
+    infinite: false,
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"><i class="icon-chevron-right"></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="icon-chevron-right"></i></button>',
+    responsive: [
+        {
+            breakpoint: 991,
+            settings: {
+                slidesToShow: 2,
+                variableWidth: false,
+                arrows: false,
+            }
+        },
+        {
+            breakpoint: 575,
+            settings: {
+                slidesToShow: 1,
+                arrows: true,
+                centerMode: true,
+                variableWidth: true,
+                dots: false,
+                infinite: true,
+            }
+        },
+    ]
+});
--- #src/js/index.js
+++ #src/js/index.js
@@ -3,11 +3,13 @@ $(document).ready(function () {
     @@include('libs/dynamic-adapt.js')
     @@include('libs/wNumb.js')
     @@include('libs/nouislider.js')
+    @@include('libs/slick.min.js')
 
     // components
     @@include('components/webp.js')
     @@include('components/menu.js')
     @@include('components/calculator.js')
+    @@include('components/slider.js')
 });
 
 @@include('components/header-fixed.js')
