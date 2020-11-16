38ab5f0066f7fd72efd852c8f97362a6d14ade02
mortalshot <seregagl1996@gmail.com>
2020-11-16 17:20:20 +0300

    outsourcing page

--- ITCulture/js/script.js
+++ ITCulture/js/script.js
@@ -3348,6 +3348,40 @@ function priceCalculate(computerPrice, serverPrice, serverNum, computerNum) {
         },
     ]
 });
+
+$('.cases__slider').slick({
+    arrows: true,
+    dots: true,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
+    infinite: true,
+    initialSlide: 1,
+    prevArrow: '<button type="button" class="slick-prev"><i class="icon-chevron-right"></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="icon-chevron-right"></i></button>',
+    responsive: [
+        {
+            breakpoint: 1300,
+            settings: {
+                slidesToShow: 1,
+                variableWidth: false,
+                centerMode: false,
+                initialSlide: 0,
+            }
+        },
+        // {
+        //     breakpoint: 575,
+        //     settings: {
+        //         slidesToShow: 1,
+        //         arrows: true,
+        //         centerMode: true,
+        //         variableWidth: true,
+        //         dots: false,
+        //         infinite: true,
+        //     }
+        // },
+    ]
+});
 });
 
 mediaQueryHeader = window.matchMedia('(min-width: 881px)');
